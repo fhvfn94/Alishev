e7e6b1795be0c84700e593a55a62cdf29bc4313b
Эльдар К. Ишмуханбетов <IshmukhanbetovEK@mcms.local>
2023-02-13 20:21:07 +0300

    lesson static constanta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Static.Human;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human.description = "Nice"; // для константы не нужен объект
+        Human h1 = new Human("ashab", 12);
+//        Human.description = "Bad";
+        Human h2 = new Human("Bill", 55);
+//        System.out.println(Math.PI);
+//        System.out.println(Human.PI);
+        h2.printNumberOfPeople();
+        Human h3 = new Human("Bar", 44);
+        h1.printNumberOfPeople();
     }
 }
